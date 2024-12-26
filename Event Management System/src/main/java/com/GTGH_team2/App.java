68933782972360dee01f579d6aecd46c0ee04d27
Nikoleta Kapsali <nikoletakapsali@hotmail.com>
2024-12-26 16:06:10 +0200

    maven project (initial structure)

---
+++ Event Management System/src/main/java/com/GTGH_team2/App.java
@@ -0,0 +1,13 @@
+package com.GTGH_team2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
---
+++ Event Management System/src/main/java/com/GTGH_team2/Organizer.java
@@ -0,0 +1,23 @@
+package com.GTGH_team2;
+
+class Organizer {
+    private String afm;
+    private String name;
+    private String surname;
+    private String description;
+    private Event events;
+
+    public Organizer(String afm, String name, String surname, String description, Event events) {
+        this.afm = afm;
+        this.name = name;
+        this.surname = surname;
+        this.description = description;
+        this.events = events;
+    }
+
+    public void addEvent() {
+        
+    }
+
+}
+
