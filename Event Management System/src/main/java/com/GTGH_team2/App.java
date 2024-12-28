65898e8650534f2013fd64a7004a9100f06e2f71
Nikoleta Kapsali <nikoletakapsali@hotmail.com>
2024-12-28 19:42:18 +0200

    update App.java

--- Event Management System/src/main/java/com/GTGH_team2/App.java
+++ Event Management System/src/main/java/com/GTGH_team2/App.java
@@ -10,11 +10,11 @@ public class App
     {
 //        Functionality try!!!
         Organizer org1 = new Organizer("111654321","Nikki","Ioannou","Computer Scientist");
-        Event event = new Event("Sailor Moon R","20 Years of Sailor Moon","The princess of the moon is going to save the world again","Athens",500,"Friday","May","2025",5,30,"5:30",org1,true);
+        Event event = new Event("Christmas Bazaar","Christmas Event","You can either sell objects or buy objects for charity","Athens",500,"Friday","December","2025",5,30,"5:30",org1,true);
         org1.addEvent(event);
 
         Organizer org2 = new Organizer("111654321","Maria","Papadopoulou","Computer Scientist");
-        Event event2 = new Event("Sailor Moon S","20 Years of Sailor Moon","The princess of the moon is going to save the world again","Athens",500,"Friday","May","2025",5,30,"5:30",org1,true);
+        Event event2 = new Event("Cinema Festival","Cinema History","Come and enjoy movies of all time","Athens",500,"Thursday","May","2025",5,30,"5:30",org1,true);
         org2.addEvent(event2);
 
         org1.addEvent(event2);
