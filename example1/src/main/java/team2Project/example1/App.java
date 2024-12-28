c629b7ebdd4bfce054573aa26e1eebe3a680cca8
Ioanna Markou <72706584+ioannanm@users.noreply.github.com>
2024-12-28 19:53:54 +0200

    Test on uploading java class of Employee on github desktop
    Connecting java code for Employee class from eclipse to github desktop.

---
+++ example1/src/main/java/team2Project/example1/App.java
@@ -0,0 +1,12 @@
+package team2Project.example1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ example1/src/main/java/team2Project/example1/Employee.java
@@ -0,0 +1,21 @@
+package team2Project.example1;
+
+public class Employee {
+	private String name;
+	private String surname;
+	private String email;
+	/**
+	 * @param name
+	 * @param surname
+	 * @param email
+	 */
+	public Employee(String name, String surname, String email) {
+		super();
+		this.name = "Joan";
+		this.surname = surname;
+		this.email = email;
+	}
+	
+	
+	
+}
