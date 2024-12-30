1b2300c90c607a31c77bdc63bb1d2c58835bcbda
Ioanna Markou <72706584+ioannanm@users.noreply.github.com>
2024-12-30 15:52:00 +0200

    Start of Employee class
    First draft of Employee class

--- Team2_Joana/src/main/java/Team2/Team2_Joana/Employee.java
+++ Team2_Joana/src/main/java/Team2/Team2_Joana/Employee.java
@@ -1,5 +1,8 @@
 package Team2.Team2_Joana;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
 	private String name;
 	private String surname;
@@ -9,11 +12,61 @@ public class Employee {
 	 * @param surname
 	 * @param email
 	 */
+	private List<String> events;
+
+	
 	public Employee(String name, String surname, String email) {
-		super();
 		this.name = name;
 		this.surname = surname;
 		this.email = email;
+		this.events = new ArrayList<>();
 	}
+	
+		
+	public boolean disapproveEvent(String event) {
+		return true;
+	}
+	
+	public boolean approveEvent(String event) {
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<String> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<String> events) {
+		this.events = events;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", surname=" + surname + ", email=" + email + ", events=" + events + "]";
+	}
+	
 	
 }
