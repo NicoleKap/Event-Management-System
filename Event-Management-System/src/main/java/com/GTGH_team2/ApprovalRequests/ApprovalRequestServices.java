4c02d1f64bf32fe586132eddbeba3ab14314ff39
Ioanna Markou <72706584+ioannanm@users.noreply.github.com>
2025-01-10 23:56:52 +0200

    Creation of methods for ApprovalRequestServices
    Creation of methods to make a request from an organizer, to accept and reject a request from an employee

--- Event-Management-System/src/main/java/com/GTGH_team2/ApprovalRequests/ApprovalRequestServices.java
+++ Event-Management-System/src/main/java/com/GTGH_team2/ApprovalRequests/ApprovalRequestServices.java
@@ -12,29 +12,26 @@ import com.GTGH_team2.Events.EventServices;
 import com.GTGH_team2.Organizers.Organizer;
 import com.GTGH_team2.Organizers.OrganizerServices;
 
-
-
-
 //the employee needs to see the pending requests so he can approve or reject them
 public class ApprovalRequestServices {
 
 	private List<ApprovalRequest> approvalRequests = new ArrayList<ApprovalRequest>();
-	private List<ApprovalRequest> allPendingRequests = new ArrayList<ApprovalRequest>(); // All requests that are pending
-	private List<ApprovalRequest> allApprovedRequests = new ArrayList<ApprovalRequest>(); // All requests that are approved
-	
+	private List<ApprovalRequest> pendingRequests = new ArrayList<ApprovalRequest>(); // All requests that are pending
+	private List<ApprovalRequest> approvedRequests = new ArrayList<ApprovalRequest>(); // All requests that are approved
+
 	OrganizerServices organizerServices;
 	EmployeeServices employeeServices;
 	EventServices eventServices;
 
-	
-	public ApprovalRequestServices(OrganizerServices organizerServices, EmployeeServices employeeServices) {
+	public ApprovalRequestServices(OrganizerServices organizerServices, EmployeeServices employeeServices,
+			EventServices eventServices) {
 		this.organizerServices = organizerServices;
 		this.employeeServices = employeeServices;
+		this.eventServices = eventServices;
 	}
 
-
 	public List<ApprovalRequest> addApprovalRequest(Event event, Organizer organizer, Employee employee) {
-		approvalRequests.add(new ApprovalRequest(1,null, event, organizer, null, null,employee, null, null));
+		approvalRequests.add(new ApprovalRequest(1, null, event, organizer, null, null, employee, null, null));
 		return approvalRequests;
 	}
 
@@ -42,63 +39,74 @@ public class ApprovalRequestServices {
 		approvalRequests.removeIf(approvalRequest -> approvalRequest.getId() == idRequest);
 		return approvalRequests;
 	}
-	
-	public List<ApprovalRequest> approveRequest(Integer idEvent, Integer idEmployee) {
-		for (Event event : eventServices.getAllEvents()) {
-			if (event.getId() == idEvent) {
-				for (Employee employee : employeeServices.getAllEmployees()) {
-					if (idEmployee == employee.getId()) {
-						addApprovalRequest(event, null, employee);
-					}
-				}
-			}
-		}
-        return approvalRequests;
-    }
-	
-//	public List<ApprovalRequest> makeRequest(Integer idEvent, Integer idOrganizer) {
+
+	public List<ApprovalRequest> getApprovalRequest() {
+		return approvalRequests;
+	}
+
+//	public List<ApprovalRequest> approveRequest1(Integer idEvent, Integer idEmployee) {
 //		for (Event event : eventServices.getAllEvents()) {
 //			if (event.getId() == idEvent) {
-//				for (Organizer organizer : organizerServices.getAllOrganizers()) {
+//				for (Employee employee : employeeServices.getAllEmployees()) {
 //					if (idEmployee == employee.getId()) {
 //						addApprovalRequest(event, null, employee);
 //					}
 //				}
 //			}
 //		}
+//		return approvalRequests;
+//	} most likely wrong
+
+	public List<ApprovalRequest> approveRequest(Integer idApprovalRequest, Integer idEmployee) {
+		for (ApprovalRequest approvalRequest : getApprovalRequest()) {
+			if (approvalRequest.getId() == idApprovalRequest) {
+				for (Employee employee : employeeServices.getAllEmployees()) {
+					if (idEmployee == employee.getId()) {
+						this.approvedRequests.add(approvalRequest); // or all events immediately
+						this.removePendingRequest(idApprovalRequest);
+						//employee.getAllRequests().add(idApprovalRequest);
+					}
+				}
+			}
+		}
+		return approvedRequests;
+	}
+
+	public List<ApprovalRequest> rejectRequest(Integer idApprovalRequest, Integer idEmployee) {
+		for (Employee employee : employeeServices.getAllEmployees()) {
+			if (idEmployee == employee.getId()) {
+				this.removeApprovalRequest(idApprovalRequest);
+				this.removePendingRequest(idApprovalRequest);
+				//employee.getAllRequests().add(idApprovalRequest);
+			}
+		}
+		return approvedRequests;
+	}
+
+	public List<ApprovalRequest> removePendingRequest(Integer idRequest) {
+		pendingRequests.removeIf(approvalRequest -> approvalRequest.getId() == idRequest);
+				return pendingRequests;
+	}
+
+	public void addPendingRequest(ApprovalRequest request) {
+		pendingRequests.add(request);
+	}
+
+	public List<ApprovalRequest> getAllPendingRequests() {
+		return pendingRequests;
+	}
+
+//	public List<ApprovalRequest> makeRequest(Integer idEvent, Integer idOrganizer, String comments) {
+//		for (Event event : eventServices.getAllEvents()) {
+//			if (event.getId() == idEvent) {
+//				for (Organizer organizer : organizerServices.getAllOrganizers()) {
+//					if (idOrganizer == organizer.getId()) {
+//						addApprovalRequest(1, "Add", event, organizer, null, "Pending", null, null, comments);
+//					}
+//				}
+//			}
+//		}
 //        return approvalRequests;
-//    }
-//	
-//
-//	
-//	public void addPendingRequest(ApprovalRequest request) {
-//		if(request.getStatus().equalsIgnoreCase("Pending"));
-//			allPendingRequests.add(request);
-//	}
-//	
-//	public List<ApprovalRequest> getAllPendingRequests() {
-//		return allPendingRequests;
-//	}
-//	
-//	public List<ApprovalRequest> removePendingRequest(ApprovalRequest request) {
-//		allPendingRequests.remove(request);
-//		return allPendingRequests;
-//	}
-//	
-//	
-//	public void addApprovalRequest(ApprovalRequest request) {
-//		allApprovalRequests.add(request);
-//	}
-//	
-//	public List<ApprovalRequest> getAllApprovalRequests() {
-//		return allApprovalRequests;
-//	}
-//	
-//	public List<ApprovalRequest> removeApprovalRequest(ApprovalRequest request) {
-//		allApprovalRequests.remove(request);
-//		return allApprovalRequests;
-//	}
-	
-	
+//    } !Need OrganizerServices
 
 }
--- Event-Management-System/src/main/java/com/GTGH_team2/Events/EventServices.java
+++ Event-Management-System/src/main/java/com/GTGH_team2/Events/EventServices.java
@@ -1,41 +1,110 @@
 package com.GTGH_team2.Events;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.GTGH_team2.Visitors.Visitor;
 
 public class EventServices {
 
-	private static final ArrayList<Event> allEvents = new ArrayList<>();
-//    private ArrayList<Visitor> participants = new ArrayList<>();
+	private List<Event> allEvents = new ArrayList<>();
+	private List<Visitor> participants = new ArrayList<>();
+	
+	public List<Event> getAllEvents() {
+		return allEvents;
+	}
 
-//    public ArrayList<Visitor> getParticipants() {
-//		return participants;
-//	}
-//
-//	public void setParticipants(ArrayList<Visitor> participants) {
-//		this.participants = participants;
-//	}
-//
-//	public static ArrayList<Event> getAllevents() {
-//		return allEvents;
-//	}
-//
-//    public void addEvent(Event event) {
-//        allEvents.add(event);
-//    }
-//
-//    public void removeEvent(Event event) {
-//        allEvents.remove(event);
-//    }
-//
-//    public static ArrayList<Event> viewEvents() {
-//        return allEvents;
-//    }
-//    
-//	public void addParticipant(Visitor visitor) {
-//        if (!participants.contains(visitor)) {
-//            participants.add(visitor);
-//        }
-//    }
+	public void setAllEvents(ArrayList<Event> allEvents) {
+		this.allEvents = allEvents;
+	}
+
+	public void setParticipants(ArrayList<Visitor> participants) {
+		this.participants = participants;
+	}
+
+	 public List<Visitor> getParticipants() {
+			return participants;
+		}
+	    
+	// Add an Event to the list
+	 
+    public void addEvent(Event event) {
+        allEvents.add(event);
+        System.out.println("The event " + event.getTitle() + " is added!");
+    }
+
+    // Remove an Event from the list
+    
+    public void removeEvent(int id) {
+    	Event eventToBeRemoved = null;
+    	for(Event event : allEvents) {
+    		if(event.getId() == id) {
+    			eventToBeRemoved = event;
+    			break;
+    		}
+    	}
+    	if(eventToBeRemoved != null) {
+			allEvents.remove(eventToBeRemoved);
+			System.out.println("The event " + eventToBeRemoved.getTitle() + " is removed successfully!");
+		}else {
+			System.out.println("The event does not exists in the list!");
+		}
+    }
+    
+    // Update an Event
+    
+    public void updateEvent(int id, String newTitle, String newTheme, String newDescription, String newLocation, int newMaxCapacity, String newDay, String newMonth, int newYear, int newHour, int newMinute, String newDuration) {
+    	for(Event event : allEvents) {
+    		if(newTitle != null)
+    			event.setTitle(newTitle);
+    		if(newTheme != null)
+    			event.setTheme(newTheme);
+    		if(newDescription != null)
+    			event.setDescription(newDescription);
+    		if(newLocation != null)
+    			event.setTitle(newLocation);
+    		if(newMaxCapacity != 0)
+    			event.setMaxCapacity(newMaxCapacity);
+    		if(newDay!= null) 
+    			event.setDay(newDay);
+    		if(newMonth != null)
+    			event.setTheme(newTheme);
+    		if(newYear != 0)
+    			event.setDescription(newDescription);
+    		if(newHour != 0)
+    			event.setTitle(newLocation);
+    		if(newMinute != 0)
+    			event.setMinutes(newMinute);
+    		if(newDuration!= null) {
+    			event.setDuration(newDuration);
+    		}
+    	}
+    }
+    
+    // Add a participant to the list
+    
+	public void addParticipant(Visitor visitor) {
+        if (!participants.contains(visitor)) {
+            participants.add(visitor);
+        }
+    }
+	
+	// Update a participant in the list
+	
+	public void updateParticipant(String newName, String newSurname, String newEmail) {
+		for(Visitor participant : participants) {
+			if(newName!= null)
+				participant.setName(newName);
+			if(newSurname!= null)
+				participant.setSurname(newSurname);
+			if(newEmail!= null)
+				participant.setEmail(newEmail);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "EventServices [allEvents=" + allEvents + "]";
+	}
+	
 }
