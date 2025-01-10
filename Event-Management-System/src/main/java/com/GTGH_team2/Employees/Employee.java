2e9c9b8acdafb931a418304dc22b08b721984352
Nikoleta Kapsali <128274910+NicoleKap@users.noreply.github.com>
2025-01-10 20:34:31 +0200

    Updated Structure
    Folders added
    Entities & Services added

---
+++ Event-Management-System/src/main/java/com/GTGH_team2/Employees/Employee.java
@@ -0,0 +1,72 @@
+package com.GTGH_team2.Employees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+	private String name;
+	private String surname;
+	private String email;
+	private List<String> events = new ArrayList<String>();
+	/**
+	 * @param name
+	 * @param surname
+	 * @param email
+	 */
+	
+	public Employee(String name, String surname, String email) {
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+	}
+	
+		
+	public boolean disapproveEvent(String event) {
+		return true;
+	}
+	
+	public boolean approveEvent(String event) {
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<String> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<String> events) {
+		this.events = events;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", surname=" + surname + ", email=" + email + ", events=" + events + "]";
+	}
+	
+	
+}
+
+
