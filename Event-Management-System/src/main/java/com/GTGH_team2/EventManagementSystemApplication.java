0be6b4c343a4190887d2364bab140fc23192c2ca
kalliopigriva <grivakalliopi@gmail.com>
2025-01-11 19:55:48 +0200

    MAIN
    prosthhkh 3 visitor

--- Event-Management-System/src/main/java/com/GTGH_team2/EventManagementSystemApplication.java
+++ Event-Management-System/src/main/java/com/GTGH_team2/EventManagementSystemApplication.java
@@ -3,11 +3,22 @@ package com.GTGH_team2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.GTGH_team2.Visitors.Visitor;
+import com.GTGH_team2.Visitors.VisitorServices;
+
 @SpringBootApplication
 public class EventManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventManagementSystemApplication.class, args);
+		VisitorServices visitorServices = new VisitorServices();
+
+        
+        visitorServices.addVisitor(new Visitor(1, "John", "A", "johnA@gmail.com"));
+        visitorServices.addVisitor(new Visitor(2, "Jim", "B", "jimB@gmail.com"));
+        visitorServices.addVisitor(new Visitor(3, "Mary", "C", "maryC@gmail.com"));
+
+        visitorServices.getAllVisitors();
 	}
 
 }
