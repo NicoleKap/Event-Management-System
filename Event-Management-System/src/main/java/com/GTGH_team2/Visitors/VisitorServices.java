9f410976c2ce757babdbc839e7c1d0bacb0ce800
kalliopigriva <grivakalliopi@gmail.com>
2025-01-11 22:14:14 +0200

    Participant delete
    we dont need a list of participants so i delete the methods

--- Event-Management-System/src/main/java/com/GTGH_team2/Visitors/VisitorServices.java
+++ Event-Management-System/src/main/java/com/GTGH_team2/Visitors/VisitorServices.java
@@ -3,12 +3,13 @@ package com.GTGH_team2.Visitors;
 	import java.util.ArrayList;
 	import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 	@Service
 	public class VisitorServices {
+		
 	    private List<Visitor> visitors = new ArrayList<Visitor>();
-	    private List<Visitor> participants = new ArrayList<Visitor>();
 
 	    public List<Visitor> getVisitors() {
 			return visitors;
@@ -22,6 +23,9 @@ import org.springframework.stereotype.Service;
 	        visitors.add(visitor);
 	        return visitors;
 	    }
+	    
+	    //teleutaios visiotr ths listas kai pairnw to od, auksanw, enas visitor diagrafetai diagrafontai kai ta reservations
+	    //2 methods delete reservations by visitor id delete reservations by event id
 		
 	    public List<Visitor> removeVisitor(int id) {
 	    	visitors.removeIf(visitor -> visitor.getId() == id);
@@ -42,23 +46,4 @@ import org.springframework.stereotype.Service;
 	        return visitors;
 	    }
 	    
-	    //Add a participant to the list
-	    public void addParticipant(Visitor visitor) {
-	    	if(!participants.contains(visitor)) {
-	    	participants.add(visitor);
-	    	}
-	    }
-	    
-	    //Update a participant in the list
-	    public void updateParticipant(int id, String newName, String newSurname, String newEmail) {
-		       for(Visitor participant : participants)
-		            if (id == participant.getId()) { 
-		                if (newName != null)
-		                	participant.setName(newName); 
-		                if (newSurname != null) 
-		                	participant.setSurname(newSurname);
-		                if (newEmail != null)
-		                	participant.setEmail(newEmail);
-		            }
-	    }
 	}
