90ec4876513f17eeccce49dca3d7840fe257583f
Nikoleta Kapsali <128274910+NicoleKap@users.noreply.github.com>
2025-01-15 12:43:12 +0200

    OrganizerServices
    1. unique Ids auto generated
    2. Unused Imports removes

--- Event-Management-System/src/main/java/com/GTGH_team2/Visitors/VisitorServices.java
+++ Event-Management-System/src/main/java/com/GTGH_team2/Visitors/VisitorServices.java
@@ -1,5 +1,48 @@
 package com.GTGH_team2.Visitors;
 
-public class VisitorServices {
+	import java.util.ArrayList;
+	import java.util.List;
 
-}
+import org.springframework.stereotype.Service;
+
+	@Service
+	public class VisitorServices {
+		
+	    private List<Visitor> visitors = new ArrayList<Visitor>();
+
+	    public List<Visitor> getVisitors() {
+			return visitors;
+		}
+
+		public List<Visitor> getAllVisitors() {
+	        return visitors;
+	    }
+
+	    public List<Visitor> addVisitor(Visitor visitor) {
+	        visitors.add(visitor);
+	        return visitors;
+	    }
+	    
+	   
+	    public List<Visitor> removeVisitor(Integer id) {
+	    	visitors.removeIf(visitor -> visitor.getId() == id);
+	//    	reservationServices.deleteReservationsByVisitorId(id);//remove reservations of visitor
+	        return visitors;
+	    }
+	  
+	    public List<Visitor> updateVisitor(Integer id, String newName, String newSurname, String newEmail) {
+	        for (Visitor visitor : visitors) {
+	            if (id == visitor.getId()) {
+	                if (newName != null)
+	                	visitor.setName(newName);
+	                if (newSurname != null) 
+	                	visitor.setSurname(newSurname);
+	                if (newEmail != null)
+	                	visitor.setEmail(newEmail);
+	            }
+	        }
+	        return visitors;
+	    }
+	    
+	    	    
+	}
