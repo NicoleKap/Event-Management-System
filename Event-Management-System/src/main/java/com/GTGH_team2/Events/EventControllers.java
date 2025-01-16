419272ef9fc37182a8362f062649cae1ac1733a4
Nikoleta Kapsali <128274910+NicoleKap@users.noreply.github.com>
2025-01-16 09:29:27 +0200

    update Controllers
    Controlles (EventControllers added)

--- Event-Management-System/src/main/java/com/GTGH_team2/Events/EventControllers.java
+++ Event-Management-System/src/main/java/com/GTGH_team2/Events/EventControllers.java
@@ -38,13 +38,36 @@ public class EventControllers {
 	
 	@PutMapping("/updateEvent")
 	public List<Event> updateEvent(@RequestParam Integer id, @RequestParam String newTitle, @RequestParam String newTheme,@RequestParam  String newDescription,@RequestParam  String newLocation, @RequestParam Integer newMaxCapacity, @RequestParam Integer newDay, Integer newMonth, @RequestParam Integer newYear, @RequestParam Integer newHour, @RequestParam Integer newMinute,@RequestParam  String newDuration){
-		eventServices.updateEvent(id, newTitle, newTheme, newDescription, newLocation, newMaxCapacity, newDay, newMonth, newYear, newHour, newMinute, newDuration);
-		return eventServices.getAllEvents();
+		return eventServices.updateEvent(id, newTitle, newTheme, newDescription, newLocation, newMaxCapacity, newDay, newMonth, newYear, newHour, newMinute, newDuration);
 	}
 	
 	@PostMapping("/bookAnEvent")
 	public List<Reservation> bookAnEvent(@RequestParam Integer visitorId, @RequestParam Integer eventId) {
 		return eventServices.bookingAnEvent(visitorId,eventId);
 	}
+	
+	@GetMapping("/getApprovedEvents") // Get all the approved events
+	public List<Event> viewApprovedEvents(){
+		return eventServices.viewApprovedEvents();
+	}
+	
+	@PutMapping("/updateLocation")
+	public List<Event> updateLocation(@RequestParam Integer idEvent, @RequestParam String newLocation) {
+		return eventServices.updateLocation(idEvent, newLocation);
+	}
+	
+	@PutMapping("/updateDescription")
+	public List<Event> updateDescription(@RequestParam Integer idEvent, @RequestParam String newDescription) {
+		return eventServices.updateDescription(idEvent, newDescription);
+	}
+	
+	@PutMapping("/updateStatus")
+	public List<Event> updateStatus(@RequestParam Integer idEvent, @RequestParam String newStatus){
+		return eventServices.updateEventStatus(idEvent, newStatus);
+	}
 
+	@GetMapping("/getSearchingEvents")
+	public List<Event> searchingEvent(@RequestParam Integer day, @RequestParam Integer month, @RequestParam Integer year, @RequestParam String theme){
+		return eventServices.searchingAnEvent(day,  month, year,  theme);
+	}
 }
